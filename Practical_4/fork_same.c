4eabd02b5766197af1037f1a07cb7013f8388519
shreyanmohapatra2006 <shreyanmohapatra2006@gmail.com>
2025-11-27 21:13:46 +0530

    Create fork_same.c

---
+++ Practical_4/fork_same.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// Same program, same code for parent and child
+int main() {
+    printf("Before fork\n");
+
+    pid_t pid = fork();      // create child process
+
+    // both parent and child execute this line
+    printf("After fork, PID = %d\n", getpid());
+
+    return 0;
+}
