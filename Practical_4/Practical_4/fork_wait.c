3d6ecd1dbdab3933778ad38e60273aa6e6fffd6c
shreyanmohapatra2006 <shreyanmohapatra2006@gmail.com>
2025-11-27 21:15:07 +0530

    Create fork_wait.c

---
+++ Practical_4/Practical_4/fork_wait.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+// Parent waits for child to finish
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // child process
+        printf("Child running...\n");
+        sleep(2);                      // simulate some work
+        printf("Child finished.\n");
+    } else if (pid > 0) {
+        // parent process
+        wait(NULL);                    // wait for child to end
+        printf("Parent: child completed, now exiting.\n");
+    } else {
+        perror("fork");
+    }
+    return 0;
+}
